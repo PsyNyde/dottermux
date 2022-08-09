20dac8a850f2519735668105d5cce3d4a1fffa5f
SimplyVoid <wahidislamlinad@pm.me>
2022-08-09 15:12:37 +0600

    Added nvim config🥺

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,77 @@
+local ok, bufferline = pcall(require, "bufferline")
+if not ok then
+  return
+end
+
+local ok, color = pcall(require, "colors." .. _G.theme)
+if not ok then
+  return
+end
+
+bufferline.setup {
+  options = {
+    offsets = { { filetype = "NvimTree", text = "" } },
+    separator_style = { "", ""},
+    show_tab_indicators = false,
+  },
+  highlights = {
+    fill = {
+      guifg = color.base00,
+      guibg = color.base00,
+    },
+    background = {
+      guifg = color.base03,
+      guibg = color.base00,
+    },
+
+    -- buffers
+    buffer_selected = {
+      guifg = color.base05,
+      guibg = color.base00,
+      gui = "bold",
+    },
+    buffer_visible = {
+      guifg = color.base03,
+      guibg = color.base00,
+    },
+
+    -- close buttons
+    close_button = {
+      guifg = color.base03,
+      guibg = color.base00,
+    },
+    close_button_visible = {
+      guifg = color.base03,
+      guibg = color.base00,
+    },
+    close_button_selected = {
+      guifg = color.base08,
+      guibg = color.base00,
+    },
+
+    indicator_selected = {
+      guifg = color.base00,
+      guibg = color.base00,
+    },
+
+    -- modified
+    modified = {
+      guifg = color.base03,
+      guibg = color.base00,
+    },
+    modified_visible = {
+      guifg = color.base00,
+      guibg = color.base00,
+    },
+    modified_selected = {
+      guifg = color.base0B,
+      guibg = color.base00,
+    },
+
+    -- tabs
+    tab_close = {
+      guifg = color.base00,
+      guibg = color.base00,
+    },
+  },
+}
